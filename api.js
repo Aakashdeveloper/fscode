bd47468e1c718ef65d19d7734ad179ad3b1bc793
Aakash <ahanda205@gmail.com>
2020-05-03 19:20:42 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 8900;
+const port = process.env.PORT||3000; 
 const cors = require('cors');
 const moviesRouter = express.Router();
 const productRouter = express.Router()
